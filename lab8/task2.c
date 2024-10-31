39d610058d79dd3d83655deafacb30b1ab9189bc
Avadik <95495889+DvornikovAA404@users.noreply.github.com>
2024-10-31 20:03:09 +0700

    lab8 completed.

--- lab8/task2.c
+++ lab8/task2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-void ent_matrix(int **arr, int N){
+void ent_matrix(float **arr, int N){
     for(int x = 0; x<N; x++){
         for(int y = 0; y<N+1; y++){
             printf("[%d, %d] = ", x, y);
-            scanf("%d", &arr[x][y]);
+            scanf("%f", &arr[x][y]);
         }
     }
 }
 
-void crmat(int **arr, int n){
+void crmat(float **arr, int n){
     for(int i = 0; i<n; i++){
-        arr[i] = calloc(1, sizeof(int)*(n+1));
+        arr[i] = calloc(1, sizeof(float)*(n+1));
     }
 }
 
@@ -30,70 +31,150 @@ void rep_matrix(int **b, int **arr, int N, int m){
     }
 }
 
-void show_matrix(int **arr, int N){
+void show_matrix(float **arr, int N){
     for(int i = 0; i<N; i++){
-        for(int j = 0; j<N; j++){
-            printf("%d ", arr[i][j]);
+        for(int j = 0; j<N+1; j++){
+            printf("%f ", arr[i][j]);
         }
         printf("\n");
     }
 }
 
-int d2x2(int **arr){
+void trim_matrix(int **b, int **arr, int N, int m){
+    int x = 0;
+    int y = 0;
+    for(int i = 0; i<N; i++){
+        if(i!=m){
+            for(int j=0; j<N; j++){
+                if(j!=m){
+                    b[y][x] = arr[i][j];
+                    x += 1;
+                    if(x==N-1){
+                        x = 0;
+                        y += 1;
+                    }
+                }
+            }
+        }
+    }
+}
+
+float d2x2(float **arr){
     return arr[0][0]*arr[1][1]-arr[0][1]*arr[1][0];
 }
 
-int d3x3(int **a){
+float d3x3(float **a){
     return a[0][0]*a[1][1]*a[2][2]+a[0][1]*a[1][2]*a[2][0]+a[0][2]*a[1][0]*a[2][1]-a[0][2]*a[1][1]*a[2][0]-a[0][1]*a[1][0]*a[2][2]-a[0][0]*a[1][2]*a[2][1];
-
 }
 
-float result(int **arr, int N, int m){
+// int dnxn(int **a, int N){
+//     int i = 0;
+//     int sum = 0;
+//     int k = 1;
+//     for(int l = 0; l<N; l++){
+//         k = k * a[l][l];
+//     }
+//     sum += k;
+
+//     for(int u = 1; u<N; u++){
+//         k = 1;
+//         for(int l = u, i = 0; l<N; l++){
+//             i = l-u;
+//             k = k * a[i][l];
+//         }
+//         int x = N-u;
+//         for(int ku = 0; ku<u; ku++){
+//             k = k * a[x][ku];
+//             x+=1;
+//         }
+//         sum += k;
+//     }
+//     k = 1;
+//     for(int l = 0; l<N; l++){
+//         k = k * a[(N-1)-l][l];
+//     }
+//     sum += k*(-1);
+
+//     for(int u = 1; u<N; u++){
+//         k = 1;
+//         for(int l = u, i = 1; l<N; l++, i++){
+//             k = k * a[(N)-l][N-1-l];
+//         }
+//         int x = N-u;
+//         for(int ku = 0; ku<u; ku++){
+//             k = k * a[x][(N-1)-ku];
+//             x+=1;
+//         }
+//         sum += k*(-1);
+
+//     }
+
+//     return sum;
+// }
+//
+
+float result(float **arr, int N, int m){
     if(N==1){
         return arr[0][0];
     }
     else if(N==2){
-        int **array = calloc(1, sizeof(int*)*2);
+        float **array = calloc(1, sizeof(float*)*2);
         crmat(array, N);
-        int d = d2x2(arr);
+        float d = d2x2(arr);
         rep_matrix(array, arr, N, m);
-        int dx = d2x2(array);
+        float dx = d2x2(array);
+        if(d == 0){
+            printf("Нет единственного решения!\n");
+            exit(0);
+        }
         float x = dx / (float) d;
         return x;
     }
     else if(N==3){
-        int **array = calloc(1, sizeof(int*)*2);
+        float **array = calloc(1, sizeof(float*)*2);
         crmat(array, N);
         rep_matrix(array, arr, N, m);
-        int d = d3x3(arr);
-        int dx = d3x3(array);
+        float d = d3x3(arr);
+        float dx = d3x3(array);
+        if(d == 0){
+            printf("Нет единственного решения!\n");
+            exit(0);
+        }
         float x = dx / (float) d;
         return x;
     }
+    // else{
+    //     int d = 0;
+    //     int **array = calloc(1, sizeof(int*)*N);
+    //     crmat(array, N);
+    //     rep_matrix(array, arr, N, m);
+    //     d = dnxn(arr, N);
+    //     printf("!!! %d\n", d);
+    // }
 }
 
-void shres(int **arr, int N){
-    int ans;
+void shres(float **arr, int N){
+    float ans;
     for(int i = 0; i<N; i++){
         ans = result(arr, N, i);
         if(N<=3){
             switch (i)
             {
             case 0:
-                printf("x = %d; ", ans);
+                printf("x = %f; ", ans);
                 break;
             case 1:
-                printf("y = %d; ", ans);
+                printf("y = %f; ", ans);
                 break;
             case 2:
-                printf("z = %d; ", ans);
+                printf("z = %f; ", ans);
                 break;
             default:
                 break;
             }
         }
         else{
-            printf("x%d = %d; ", i, ans);
+            printf("x%d = %f; ", i, ans);
         }
     }
     printf("\n");
@@ -103,10 +184,12 @@ int main(){
     int N = 1;
     printf("N: ");
     scanf("%d", &N);
-    int **matrix = calloc(1, sizeof(int*)*N);
+    float **matrix = calloc(1, sizeof(float*)*N);
     crmat(matrix, N);
     ent_matrix(matrix, N);
+
     show_matrix(matrix, N);
+
     printf("========================\n");
     shres(matrix, N);
 }
